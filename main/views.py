5670ea61aa74e3126268f691f501bbb5975d4d54
Mustafa Erkan Başar <mustafaerkanbasar@gmail.com>
2016-08-24 02:01:34 +0200

    Initial Commit

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import render_to_response
+
+from django.conf import settings
+
+
+def handler404(request):
+	response = render_to_response('404.html', {},context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
+
+def handler500(request):
+	response = render_to_response('500.html', {},context_instance=RequestContext(request))
+	response.status_code = 500
+	return response
+
+
+
+class Home(View):
+
+	def get(self, request):
+
+
+		return render(request, 'index.html', {
+		})
